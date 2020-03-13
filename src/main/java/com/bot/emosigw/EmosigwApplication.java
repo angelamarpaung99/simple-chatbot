3e3a677b3096177a8c7f08a7f70b1f264586c459
Angela Marpaung <angelamarpaung99@gmail.com>
2020-03-13 13:40:20 +0700

    initial commit

---
+++ src/main/java/com/bot/emosigw/EmosigwApplication.java
@@ -0,0 +1,13 @@
+package com.bot.emosigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosigwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmosigwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/emosigw/EmosigwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.emosigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosigwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
