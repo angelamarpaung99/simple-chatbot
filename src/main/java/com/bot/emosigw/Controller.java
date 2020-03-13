7a78538a08bfb697d5ca8246eab6ab2f48d1ecdc
Angela Marpaung <angelamarpaung99@gmail.com>
2020-03-13 13:47:27 +0700

    implementasi reply message

--- src/main/java/com/bot/emosigw/Controller.java
+++ src/main/java/com/bot/emosigw/Controller.java
@@ -4,6 +4,12 @@ package com.bot.emosigw;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.client.LineSignatureValidator;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
+import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,6 +18,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class Controller {
@@ -39,7 +48,18 @@ public class Controller {
             EventsModel eventsModel = objectMapper.readValue(eventsPayload, EventsModel.class);
 
             eventsModel.getEvents().forEach((event)->{
-                // kode reply message disini
+                if (event instanceof MessageEvent) {
+                    MessageEvent messageEvent = (MessageEvent) event;
+                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+                    replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+//                    replySticker(messageEvent.getReplyToken(), "1", "1");
+
+//                    List<Message> msgArray = new ArrayList<>();
+//                    msgArray.add(new TextMessage(textMessageContent.getText()));
+//                    msgArray.add(new StickerMessage("1", "106"));
+//                    ReplyMessage replyMessage = new ReplyMessage(messageEvent.getReplyToken(), msgArray);
+//                    reply(replyMessage);
+                }
             });
 
             return new ResponseEntity<>(HttpStatus.OK);
@@ -47,5 +67,25 @@ public class Controller {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    private void reply(ReplyMessage replyMessage) {
+        try {
+            lineMessagingClient.replyMessage(replyMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void replyText(String replyToken, String messageToUser){
+        TextMessage textMessage = new TextMessage(messageToUser);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, textMessage);
+        reply(replyMessage);
+    }
+
+    private void replySticker(String replyToken, String packageId, String stickerId){
+        StickerMessage stickerMessage = new StickerMessage(packageId, stickerId);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, stickerMessage);
+        reply(replyMessage);
     }
 }
