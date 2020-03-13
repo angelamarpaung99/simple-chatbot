9b509375a93d37d654c994b30153c61bca91b94d
Angela Marpaung <angelamarpaung99@gmail.com>
2020-03-13 14:37:34 +0700

    implementasi content API

--- src/main/java/com/bot/emosigw/Application.java
+++ src/main/java/com/bot/emosigw/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmosigwApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmosigwApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
